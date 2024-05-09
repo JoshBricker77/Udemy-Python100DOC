948fa7a1b353be166fc3122a47d829821e52f1ca
Joshua Bricker <joshbricker77@gmail.com>
2024-05-09 14:02:39 -0400

    Add files via upload
    a program that counts the number of letters in a string and prints the amount.

---
+++ 1.12.py
@@ -0,0 +1,6 @@
+#program for counting number of letters in your name
+print("***How many letters are in your name program***")
+#assigning name to input 
+name = input("What is your name?")
+#printing the length(len)(number of letters in string) and utilizing (name) as the input
+print(len(name))
